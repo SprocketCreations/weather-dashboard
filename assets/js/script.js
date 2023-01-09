011cb3ff09719bab44fe20845a98086f974edbdd
Kailen James <sevenflags57@gmail.com>
2023-01-09 12:17:42 -0800

    Added icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const SEARCH_FORM = document.querySelector("#search-form");
 const SEARCH_FORM_ENTRY = SEARCH_FORM.querySelector("#search-form-entry");
 const SEARCH_FORM_SUBMIT = SEARCH_FORM.querySelector("#search-form-submit");
+const CARDS_ROOT = document.querySelector("#cards");
 
 const MAX_HISTORY_LENGTH = 10;
 const WEATHER_CARDS = [];
@@ -8,14 +9,16 @@ const WEATHER_CARDS = [];
 const API = (latitude, longitude, key = "0369d7a1c84205562665deb9dec64eea") => `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}&units=imperial`;
 const GEO_API = (city, limit = 1, key = "0369d7a1c84205562665deb9dec64eea") => `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${key}`;
 
-function WeatherCard(header, temp_span, wind_span, humidity_span) {
+function WeatherCard(header, img, temp_span, wind_span, humidity_span) {
 	this.header = header;
+	this.img = img;
 	this.temp_span = temp_span;
 	this.wind_span = wind_span;
 	this.humidity_span = humidity_span;
 
-	this.updateCard = (date, temperature, wind, humidity) => {
+	this.updateCard = (date, icon, temperature, wind, humidity) => {
 		this.header.textContent = date;
+		this.img.src = icon;
 		this.temp_span.textContent = temperature;
 		this.wind_span.textContent = wind;
 		this.humidity_span.textContent = humidity;
@@ -98,14 +101,17 @@ const getWeather = (lat, lon, city) => {
 	doFetch(API(lat, lon), json => {
 		const weathers = filterWeatherData(json.list);
 
+		CARDS_ROOT.classList.remove("invisible");
+
 		for (let i = 0; i < 6; ++i) {
 			const weather = weathers[i];
+			const icon = `http://openweathermap.org/img/w/${weathers[i].weather[0].icon}.png`;
 			const date = `${i === 0 ? `${city} (` : ""}${dayjs.unix(weather.dt).format("M/D/YYYY")}${i === 0 ? ")" : ""}`;
 			const temperature = `${weather.main.temp}*F`;
 			const wind = `${weather.wind.speed} MPH`;
 			const humidity = `${weather.main.humidity}%`;
 
-			WEATHER_CARDS[i].updateCard(date, temperature, wind, humidity);
+			WEATHER_CARDS[i].updateCard(date, icon, temperature, wind, humidity);
 		}
 	});
 };
@@ -115,21 +121,20 @@ const doFetch = (api, callback) => {
 };
 
 const linkCards = () => {
-	const root = document.querySelector("#cards");
+	
 	const cards = [];
-	cards.push(root.querySelector("div"));
-	cards.push(...root.querySelectorAll(":scope > div > div"));
+	cards.push(CARDS_ROOT.querySelector(":scope > div"));
+	cards.push(...CARDS_ROOT.querySelectorAll(":scope > div:not(:first-child) > div"));
 
 	for (let i = 0; i < cards.length; ++i) {
-		const header = cards[i].querySelector(`:scope > ${i === 0 ? "h3" : "h5"}`)
+		const header = cards[i].querySelector(`:scope ${i === 0 ? "h3" : "h5"}`)
 		const spans = [...cards[i].querySelectorAll(":scope span")];
-		if (i !== 0) { spans.splice(0, 1); }
-		WEATHER_CARDS.push(new WeatherCard(header, spans[0], spans[1], spans[2]));
+		const img = cards[i].querySelector(":scope img");
+		WEATHER_CARDS.push(new WeatherCard(header, img, spans[0], spans[1], spans[2]));
 	}
 };
 
 const filterWeatherData = weatherData => {
-	//console.log(weatherData);
 	const filteredWeatherData = [];
 	weatherData.forEach(data => {
 		const hour = dayjs.unix(data.dt).hour();
